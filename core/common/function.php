b89e8177b22e56a6ea51b2864f35762ec6a1e5b0
LongDH <164897033@qq.com>
2018-10-19 22:58:10 +0800

    添加返回json格式公共函数

--- core/common/function.php
+++ core/common/function.php
@@ -4,7 +4,7 @@
  * @Author: LongDH
  * @Date:   2017-11-04 21:18:46
  * @Last Modified by:   LongDH
- * @Last Modified time: 2018-10-19 16:31:03
+ * @Last Modified time: 2018-10-19 22:57:34
  */
 
 /**
@@ -69,44 +69,44 @@ function http_get($url, $timeOut = 5, $connectTimeOut = 5) {
  * @return string content
  */
 function http_post($url, $param, $timeOut = 5, $connectTimeOut = 5) {
-     $oCurl = curl_init ();
-     if (stripos ( $url, "http://" ) !== FALSE || stripos ( $url, "https://" ) !== FALSE) {
-          curl_setopt ( $oCurl, CURLOPT_SSL_VERIFYPEER, FALSE );
-          curl_setopt ( $oCurl, CURLOPT_SSL_VERIFYHOST, false );
+    $oCurl = curl_init ();
+    if (stripos ( $url, "http://" ) !== FALSE || stripos ( $url, "https://" ) !== FALSE) {
+        curl_setopt ( $oCurl, CURLOPT_SSL_VERIFYPEER, FALSE );
+        curl_setopt ( $oCurl, CURLOPT_SSL_VERIFYHOST, false );
      }
-     if (is_string ( $param )) {
-          $strPOST = $param;
-     } else {
-          $aPOST = array ();
-          foreach ( $param as $key => $val ) {
-               $aPOST [] = $key . "=" . urlencode ( $val );
-          }
-          $strPOST = join ( "&", $aPOST );
-     }
-     curl_setopt($oCurl, CURLOPT_URL, $url );
-     curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1 );
-     curl_setopt($oCurl, CURLOPT_POST, true );
-     curl_setopt($oCurl, CURLOPT_FOLLOWLOCATION, true);
-     curl_setopt($oCurl, CURLOPT_POSTFIELDS, $strPOST );
-     curl_setopt($oCurl, CURLOPT_TIMEOUT, $timeOut);
-     curl_setopt($oCurl, CURLOPT_CONNECTTIMEOUT, $connectTimeOut);
-     $sContent = curl_exec ($oCurl );
-     $aStatus = curl_getinfo ($oCurl );
-     $error = curl_error($oCurl );
-     curl_close ($oCurl );
-     if (intval ($aStatus ["http_code"] ) == 200) {
-          return array(
-                    'status' => true,
-                    'content' => $sContent,
-                    'code' => $aStatus ["http_code"],
+    if (is_string ( $param )) {
+        $strPOST = $param;
+    } else {
+        $aPOST = array ();
+        foreach ( $param as $key => $val ) {
+            $aPOST [] = $key . "=" . urlencode ( $val );
+        }
+        $strPOST = join ( "&", $aPOST );
+    }
+    curl_setopt($oCurl, CURLOPT_URL, $url );
+    curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1 );
+    curl_setopt($oCurl, CURLOPT_POST, true );
+    curl_setopt($oCurl, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($oCurl, CURLOPT_POSTFIELDS, $strPOST );
+    curl_setopt($oCurl, CURLOPT_TIMEOUT, $timeOut);
+    curl_setopt($oCurl, CURLOPT_CONNECTTIMEOUT, $connectTimeOut);
+    $sContent = curl_exec ($oCurl );
+    $aStatus = curl_getinfo ($oCurl );
+    $error = curl_error($oCurl );
+    curl_close ($oCurl );
+    if (intval ($aStatus ["http_code"] ) == 200) {
+        return array(
+            'status' => true,
+            'content' => $sContent,
+            'code' => $aStatus ["http_code"],
           );
-     } else {
-          return array(
-                    'status' => false,
-                    'content' => json_encode(array("error" => $error, "url" => $url)),
-                    'code' => $aStatus ["http_code"],
-          );
-     }
+    } else {
+        return array(
+            'status' => false,
+            'content' => json_encode(array("error" => $error, "url" => $url)),
+            'code' => $aStatus ["http_code"],
+        );
+    }
 }
 
 /**
@@ -115,14 +115,30 @@ function http_post($url, $param, $timeOut = 5, $connectTimeOut = 5) {
  * @return [type]      [description]
  */
 function json($arr = []) {
+    header('content-type:application/json;charset=utf8');
     if(!$arr || !is_array($arr)) {
-        return false;
+        exit('参数出错！');
     }
     $data = json_encode($arr, JSON_UNESCAPED_UNICODE);
     if (JSON_ERROR_NONE !== json_last_error()) {
         throw new RuntimeException('Unable to parse response body into JSON: ' . json_last_error());
     }
-
     return $data === null ? array() : $data;
+}
+
+/**
+ * 响应返回JSON 格式数据
+ * @param  [type] $arr [description]
+ */
+function returnJson($arr){
+    header('content-type:application/json;charset=utf8');
+    if(!$arr || !is_array($arr)) {
+        exit('参数出错！');
+    }
+    $data = json_encode($arr, JSON_UNESCAPED_UNICODE);
+    if (JSON_ERROR_NONE !== json_last_error()) {
+        throw new RuntimeException('Unable to parse response body into JSON: ' . json_last_error());
+    }
+    exit($data);
 }
 
